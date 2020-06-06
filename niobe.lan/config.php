b9d200d6bafce98edb5044242f311b67784fa1ea
root <root@gallery.localdomain>
2020-06-06 14:58:08 +0000

    added new config names for niobe

---
+++ niobe.lan/config.php
@@ -0,0 +1,24 @@
+<?php
+
+define('PROGRAM',    '/phpgallery/');
+define('CSS',        PROGRAM.'css/style.css');
+define('IMAGE_ROOT', PROGRAM.'images/');
+define('TOP',        '/');
+
+// logon
+define('LOGIN_ENABLED', True);
+define('LOGIN_PATH',    PROGRAM.'logon/');
+
+// additional pages
+//define('ALS', False);
+//define('WIREDPUSSY', False);
+
+// database password
+define('PASSWD_DB', False);
+
+define('PHPTHUMB', '/');
+define('PLUGINS_DIR', 'plugins/');
+
+require_once($_SERVER['DOCUMENT_ROOT'].PROGRAM.'include_check.php');
+
+?>
